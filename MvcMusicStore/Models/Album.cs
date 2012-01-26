47d5d6053f25b79a981503c535309476f3ff2779
JawsTheGame <tfleischer@gmail.com>
2012-01-26 17:38:17 -0500

    More work

--- MvcMusicStore/Models/Album.cs
+++ MvcMusicStore/Models/Album.cs
@@ -1,12 +1,32 @@
-﻿namespace MvcMusicStore.Models {
+﻿using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace MvcMusicStore.Models {
+    [Bind(Exclude = "AlbumId")]
 	public class Album {
+        [ScaffoldColumn(false)]
 		public int AlbumId { get; set; }
+
+        [DisplayName("Genre")]
 		public int GenreId { get; set; }
+
+        [DisplayName("Artist")]
 		public int ArtistId { get; set; }
+
+        [Required(ErrorMessage = "An Album Title is Required")]
+        [StringLength(160)]
 		public string Title { get; set; }
+
+        [Required(ErrorMessage = "Price is Required")]
+        [Range(0.01, 100.00, ErrorMessage = "Price must be between 0.01 and 100.00")]
 		public decimal Price { get; set; }
+
+        [DisplayName("Album Art URL")]
+        [StringLength(1024)]
 		public string AlbumArtUrl { get; set; }
-		public Genre Genre { get; set; }
-		public Artist Artist { get; set; }
+
+		public virtual Genre Genre { get; set; }
+		public virtual Artist Artist { get; set; }
 	}
 }
